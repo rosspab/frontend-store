1bd8bd0214021f41ed316c67694ef6ad6f75c756
Pablo Soto <filmcastudios@gmail.com>
2021-11-09 16:30:17 -0600

    Finalizado la galeria

--- festivalMusica/gulpfile.js
+++ festivalMusica/gulpfile.js
@@ -49,15 +49,23 @@ function versionAvif( done ){
     done();
 }
 
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe( dest('build/js'))
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
     done();
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
 
 //se ejecuta siempre la palabra gulp + el nombre del export en este caso css entonces seria gulp css
