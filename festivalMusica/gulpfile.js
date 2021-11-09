7a7b59ace75231cc851040ab675e882a40449c83
Pablo Soto <filmcastudios@gmail.com>
2021-11-09 15:00:38 -0600

    terminando line up

--- festivalMusica/gulpfile.js
+++ festivalMusica/gulpfile.js
@@ -5,7 +5,10 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 //images
+const cache = require('gulp-cache');
+const imagemim = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     //identificar archivo sass a compilar
@@ -16,14 +19,32 @@ function css(done){
     done();
 }
 
-function versionWebp( done ){
+function imagenes (done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache(imagemim(opciones)))
+        .pipe( dest('build/img'))
+    done();
+}
 
+function versionWebp( done ){
     const opciones = {
         quality: 50
     };
-
     src('src/img/**/*.{png,jpg}')
         .pipe( webp(opciones) )
+        .pipe( dest('build/img'))
+    done();
+}
+
+function versionAvif( done ){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
         .pipe( dest('build/img'))
     done();
 }
@@ -34,7 +55,9 @@ function dev(done){
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
 
 //se ejecuta siempre la palabra gulp + el nombre del export en este caso css entonces seria gulp css
