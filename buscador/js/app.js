632a30ebe5f7e770de24254213b069046d979440
Pablo Soto <filmcastudios@gmail.com>
2022-01-24 21:00:52 -0600

    DesarrolloCompleto

--- buscador/js/app.js
+++ buscador/js/app.js
@@ -110,7 +110,21 @@ function filtrarAuto(){
     const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
     //console.log(resultado);
 
-    mostrarAutos(resultado);
+    if(resultado.length){
+        mostrarAutos(resultado);
+    }else{
+        noResultado();
+    }
+}
+
+function noResultado(){
+
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados, intenta con otros terminos de busqueda';
+    resultado.appendChild(noResultado);
 }
 
 function filtrarMarca(auto){
