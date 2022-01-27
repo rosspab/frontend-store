747d4d40e05a4f88a6c910b707931647868f0112
Pablo Soto <filmcastudios@gmail.com>
2022-01-26 19:42:25 -0600

    Local Storage en Tienda Online Listo

--- carritoCompras/js/app.js
+++ carritoCompras/js/app.js
@@ -11,6 +11,12 @@ function cargarEventListeners(){
     listaCursos.addEventListener('click', agregarCurso);
     //Elimina cursos del carrito
     carrito.addEventListener('click', eliminarCurso);
+    //Muestra los cursos de Local Storage
+    document.addEventListener('DOMContentLoaded', () => {
+        articulosCarrito = JSON.parse( localStorage.getItem('carrito') ) || [];
+
+        carritoHTML();
+    });
     //Vaciar carrito
     vaciarCarritoBtn.addEventListener('click', () => {
         articulosCarrito = []; //reseteamos el arreglo
@@ -102,6 +108,13 @@ function carritoHTML() {
         // Agrega el HTML del carrito en el tbody
         contenedorCarrito.appendChild(row);
     });
+
+    // Agregar el carrito de compras al storage
+    sincronizarStorage();
+}
+
+function sincronizarStorage() {
+    localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
 }
 
 //Elimina los cursos del tbody
